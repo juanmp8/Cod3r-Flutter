44e66119a35a273e42e55f7b7a2a30ee54f0ec4b
Juan <juanmendespeixoto@gmail.com>
2024-01-23 18:10:09 -0300

    passando dados via construtor

---
+++ multiscreens/lib/screens/categories_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import '../components/category_item.dart';
+import '../data/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'MultiScreens',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        //backgroundColor: Colors.blue,
+        centerTitle: true,
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(20),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES.map((cat) {
+          return CategoryItem(cat);
+        }).toList(),
+      ),
+    );
+  }
+}
